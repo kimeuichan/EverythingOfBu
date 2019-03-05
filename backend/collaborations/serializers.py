ba30bc411b65017880e0bd554d7447eca30ed9db
kimeuichan <eck7965@gmail.com>
2019-03-05 23:27:12 +0900

    adding permission

--- backend/collaborations/serializers.py
+++ backend/collaborations/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from .models import Post, DetailMember
 from django.contrib.auth.models import User
+from rest_framework import permissions
 
 class PostSerializer(serializers.ModelSerializer):
+    writter = serializers.ReadOnlyField(source='writter.username')
+    permission_classes = (permissions.IsAdminUser,)
     class Meta:
         model = Post
         fields = '__all__'
